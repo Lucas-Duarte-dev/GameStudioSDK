b846a4a174abaaa2b6385aced819b90e9f40a7cb
Lucas Duarte <lucasduarteb2000@hotmail.com>
2021-11-15 00:01:54 -0300

    feat(app): criando a tela para apresentação de informações de um jogo

--- AndroidStudioProjects/GameStudio/app/src/main/java/com/example/gamestudio/HomePage.java
+++ AndroidStudioProjects/GameStudio/app/src/main/java/com/example/gamestudio/HomePage.java
@@ -1,7 +1,11 @@
 package com.example.gamestudio;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home_page);
+
+        Intent intention = new Intent(this, Game.class);
+
+        Button button = (Button) findViewById(R.id.button_see_more_ori);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(intention);
+            }
+        });
     }
 }
